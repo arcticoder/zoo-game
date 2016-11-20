3429b06d41d1a711f9cee1f92866cb99e106765f
arcticoder <arcticoder@caramail.com>
2016-11-19 22:53:39 -0800

    json responses for services

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -23,24 +23,37 @@ class AnimalController extends Controller
      */
     public function incrementHunger() {
         $animals = Animal::where('state', '!=', 'dead')->get();
+        $died = 0;
+        $dying = 0;
 
         foreach ($animals as $animal) {
             if ($animal->title == 'elephant' && $animal->health < 0.7) {
                 $animal->state = 'dead';
+                $died++;
             } else {
                 $animal->health -= (rand(0, 20) / 100);
 
                 if ($animal->title == 'elephant' && $animal->health < 0.7) {
                     $animal->state = 'cannot_walk';
+                    $dying++;
                 } else if ($animal->title == 'monkey' && $animal->health < 0.3) {
                     $animal->state = 'dead';
+                    $died++;
                 } else if ($animal->title == 'giraffe' && $animal->health < 0.5) {
                     $animal->state = 'dead';
+                    $died++;
+                } else {
+                    $dying++;
                 }
             }
 
             $animal->save();
         }
+
+        return response()->json([
+            'died' => $died,
+            'dying' => $dying
+        ]);
     }
 
     /**
@@ -50,12 +63,22 @@ class AnimalController extends Controller
      */
     public function revive() {
         $animals = Animal::all();
+        $revived = 0;
 
         foreach ($animals as $animal) {
+            if ($animal->state == 'dead') {
+                $revived++;
+            }
+
             $animal->state = 'can_walk';
             $animal->health = 1.0;
             $animal->save();
         }
+
+        return response()->json([
+            'revived' => $revived,
+            'total' => sizeof($animals)
+        ]);
     }
 
     /**
@@ -67,6 +90,8 @@ class AnimalController extends Controller
         $animals = Animal::where('state', '!=', 'dead')
             ->where('health', '<', 1.0)
             ->get();
+        $fed = 0;
+        $full = 0;
 
         foreach ($animals as $animal) {
             $animal->health += (rand(10, 25) / 100);
@@ -75,7 +100,17 @@ class AnimalController extends Controller
                 $animal->health = 1.0;
             }
 
+            if ($animal->health == 1.0) {
+                $full++;
+            }
+
+            $fed++;
             $animal->save();
         }
+
+        return response()->json([
+            'fed' => $fed,
+            'full' => $full
+        ]);
     }
 }
